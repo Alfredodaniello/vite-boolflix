0e16dc026ff4d751c70b04e984e9fcb28d75a407
Alfredodaniello <alfredo_daniello@libero.it>
2024-04-17 21:08:58 +0200

    feat: add v-model + emit à function for api

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,40 @@
 <script>
+import axios from 'axios';
+import { store } from './store.js';
 import AppSearch from "./components/AppSearch.vue"
 export default {
   name: 'App',
   components: {
     AppSearch
-  }
+  },
+  data() {
+        return {
+            store,
+        };
+    },
+    methods: {
+        getFilms() {
+            const queryParams = {
+                query: store.search
+            }
+            
+            let apiUrl = 'https://api.themoviedb.org/3/search/movie?api_key=b3e4b273d1b542085c09ffe28f143f8b';
+            axios.get(apiUrl, {
+                params: queryParams
+            })
+            .then((response) => {
+
+                    store.films = response.data.results
+                    console.log(store.films)}
+                    )
+        }
+    },
 }
+
 </script>
 
 <template>
-  <AppSearch></AppSearch>
+  <AppSearch @searchPerformed="getFilms"></AppSearch>
 </template>
 
 <style>
--- src/components/AppSearch.vue
+++ src/components/AppSearch.vue
@@ -1,11 +1,22 @@
 <script>
+import { store } from '../store.js';
+import axios from 'axios';
 export default {
-    name: "AppSearch"
+    name: "AppSearch",
+    data() {
+        return {
+            store,
+        };
+    }
 }
+    
+
 </script>
 
 <template>
-ciao ciao
+
+<input type="search" v-model="store.search">
+<button @click="$emit('searchPerformed')">Cerca!</button>
 </template>
 
 <style scoped lang="scss">
